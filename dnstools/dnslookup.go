3c7279c726238b5378500026100231c0b1d50d32
李立国 <liguo1@staff.weibo.com>
2020-08-25 17:59:06 +0800

    ...

---
+++ dnstools/dnslookup.go
@@ -0,0 +1,69 @@
+package dnstools
+
+import (
+	"errors"
+	"net"
+	"strings"
+)
+
+package dnstools
+
+import (
+"errors"
+"net"
+"strings"
+)
+
+type Record struct {
+	Domain     string
+	Host       string
+	RecordType string
+}
+
+func New(domain, RecordType string) *Record {
+	return &Record{
+		Domain:     domain,
+		RecordType: RecordType,
+	}
+}
+
+func (re *Record) getCNAME() error {
+	result, err := net.LookupCNAME(re.Domain)
+	if err != nil {
+		return  err
+	}
+	if result != re.Domain + "." {
+		re.Host = result
+	}
+	return nil
+}
+
+func (re *Record) getA() error {
+	result, err := net.LookupHost(re.Domain)
+	if err != nil {
+		return err
+	}
+	re.Host = strings.Join(result, " ")
+	return nil
+}
+
+func (re *Record) GetRecordInfo() error {
+	switch {
+	case re.RecordType == "A":
+		err := re.getA()
+		if err != nil {
+			return err
+		}
+	case re.RecordType == "CNAME":
+		err := re.getCNAME()
+		if err != nil {
+			return err
+		}
+	default:
+		return errors.New("type error")
+	}
+
+	return nil
+}
+
+
